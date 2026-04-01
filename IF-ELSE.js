01f963fb473fe05cafeaa99cf3eb3dcd8f87b811
developerJatin7 <jatinsuryawanshi725@gmail.com>
2026-04-01 16:15:28 +0530

    new file code

---
+++ IF-ELSE.js
@@ -0,0 +1,127 @@
+// const a = 10;
+// if (a > 0){
+//     console.log("Positive");
+// }
+// else if (a < 0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("Zero");
+// }
+
+// const b = 5;
+// if (b%2===0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+// const age = 25;
+// if (age>=18){
+//     console.log("You are an adult.");
+// }
+// else{
+//     console.log("You are a minor.");
+// }
+
+// let a = 10;
+// let b = 20;
+// let c = 5;
+// if (a > b && a > c) {
+//     console.log("a is the largest");
+// }
+// else if (b>a && b>c){
+//     console.log("b is the largest");
+// }
+// else{   
+//      console.log("c is the largest");
+// }
+
+// let a = 10;
+// if(a%3===0 && a%5===0){
+//     console.log("it divisible by both 3 and 5");
+// }
+// else{
+//     console.log("it is not divisible by both 3 and 5");
+// }
+
+// let year = 2020;
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " is a leap year.");
+// } else {
+//     console.log(year + " is not a leap year.");
+// }
+
+// let char = 'A';
+// if (char=== 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U' || char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//     console.log(`${char} is a vowel.`);
+// } else {
+//     console.log(`${char} is a consonant.`);
+// }
+
+// let marks = 85;
+// if (marks >= 40){
+//     console.log("Pass");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//     console.log("Grade A");
+// }
+// else if (marks >= 80) {
+//     console.log("Grade B");
+// }
+// else if (marks >= 70) {
+//     console.log("Grade C");
+// }
+// else {
+//     console.log("Grade D");
+// }
+
+// let num = 15;
+// if(num>=10 && num<=50){
+//     console.log("The number is between 10 and 50.");
+// }
+// else{
+//     console.log("The number is not between 10 and 50.");
+// }
+
+// let num1 = 10;
+// let num2 = 0;
+// let operator = '/';
+// if (operator === '+') {
+//     console.log(num1 + num2);
+// }
+// else if (operator === '-') {
+//     console.log(num1 - num2);
+// }
+// else if (operator === '*') {
+//     console.log(num1*num2);
+// }
+// else if (operator === '/') {
+//     if(num2!==0){
+//         console.log(num1/num2);
+//     }
+//     else{
+//         console.log("Cannot divide by zero");
+//     }
+// }
+// else{
+//     console.log("Invalid operator");
+// }
+
+// let side1 = 3;
+// let side2 = 4;
+// let side3 = 5;
+// if (side1 + side2 > side3){
+//     console.log("The sides can form a triangle.");
+// }
+// else{
+//     console.log("The sides cannot form a triangle.");
+// }
+
