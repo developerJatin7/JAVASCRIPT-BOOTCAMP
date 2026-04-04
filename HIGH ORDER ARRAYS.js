1921b64eb2ac7236622eee7dbb03b41abe35c731
developerJatin7 <jatinsuryawanshi725@gmail.com>
2026-04-04 14:25:15 +0530

    new practice

---
+++ HIGH ORDER ARRAYS.js
@@ -0,0 +1,74 @@
+// const users = [
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 30 },
+//     { name: 'Charlie', age: 35 }
+// ];
+// const updatedUsers = users.map(user => {
+//     return `${user.name} is ${user.age}`;
+// });
+// console.log(updatedUsers);
+
+// const users = [
+//   { name: "Jatin", isActive: true },
+//   { name: "Rahul", isActive: false }
+// ];
+// const activeUsers = users.map(user => {
+//     if (user.isActive === true) {
+//         return user.status = "Active";
+//     }
+//     else {
+//         return user.status = "Inactive";
+//     }
+// });
+// console.log(users);
+
+// const products = [
+//   { name: "Phone", price: 1000 },
+//   { name: "Laptop", price: 50000 }
+// ];
+// const discountedProducts = products.map(product => {
+//     return {
+//         ...product,
+//         discountedProducts: product.price * 1.18
+//     }
+// });
+// console.log(discountedProducts);
+
+// const numbers = [10, 20, 30, 40, 50];
+// const newnums = numbers.map((number, index) => {
+//     return number + index;
+// });
+// console.log(newnums);
+
+// const users = [
+//   { name: "Jatin", address: { city: "Mumbai" } },
+//   { name: "Rahul", address: { city: "Delhi" } }
+// ];
+// const cities = users.map(user=> {
+//     return {
+//         ...user,
+//        city: user.address.city.toUpperCase(),
+//     }
+// });
+// console.log(cities);
+ 
+// const numbers = [1, 2, 3, 4, 5];
+// const newnums = numbers.map(number => {
+//     if (number % 2 === 0) {
+//         return number * 2;
+//     }
+//     else {
+//         return number 
+//     }
+// });
+// console.log(newnums);
+
+const nums = [1, 2, 3];
+
+const result = nums.map(num => {
+  if (num > 1) return num * 2;
+  else return num;
+});
+
+console.log(result);
+  
